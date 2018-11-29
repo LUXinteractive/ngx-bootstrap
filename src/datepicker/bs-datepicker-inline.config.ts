2b95ceb941f42f8b055b6c234222a6657bfd47fe
Kevin Wall <kevin.wall@luxinteractive.com>
2018-11-29 11:27:14 -0500

    Add inline datepicker.

---
+++ src/datepicker/bs-datepicker-inline.config.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+import { BsDatepickerConfig } from './bs-datepicker.config';
+
+@Injectable()
+export class BsDatepickerInlineConfig extends BsDatepickerConfig {
+}
